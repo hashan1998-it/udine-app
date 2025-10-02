2ca435a19a271450610a91530ca3986b234bd8bc
Hashan Danushka <73052374+hashan1998-it@users.noreply.github.com>
2025-10-02 11:50:35 +0530

    enhance TabLayout with custom tab bar styles and background; update COLORS for consistency

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -13,23 +13,49 @@ const TabLayout = () => {
         sceneStyle: { backgroundColor: "white" },
         tabBarActiveTintColor: COLORS.tabBarActiveTintColor,
         tabBarInactiveTintColor: COLORS.tabBarInactiveTintColor,
+        tabBarStyle: {
+          borderTopWidth: 0,
+        },
+        tabBarLabelStyle: {
+          fontSize: 14,
+        },
+        tabBarBackground: () => (
+          <View
+            style={{
+              backgroundColor: COLORS.tabBarBackgroundColor,
+              flex: 1,
+            }}
+          />
+        ),
       }}
     >
       <Tabs.Screen
         name="tables"
-        options={{ title: "Tables", tabBarIcon: ({ color }) => <Icons.TableIcon color={color} /> }}
+        options={{
+          title: "Tables",
+          tabBarIcon: ({ color }) => <Icons.TableIcon color={color} />,
+        }}
       />
       <Tabs.Screen
         name="orders"
-        options={{ title: "Orders", tabBarIcon: ({ color }) => <Icons.OrderIcon color={color} /> }}
+        options={{
+          title: "Orders",
+          tabBarIcon: ({ color }) => <Icons.OrderIcon color={color} />,
+        }}
       />
       <Tabs.Screen
         name="menu"
-        options={{ title: "Menu", tabBarIcon: ({ color }) => <Icons.MenuIcon color={color} /> }}
+        options={{
+          title: "Menu",
+          tabBarIcon: ({ color }) => <Icons.MenuIcon color={color} />,
+        }}
       />
       <Tabs.Screen
         name="report"
-        options={{ title: "Report", tabBarIcon: ({ color }) => <Icons.ReportIcon color={color} /> }}
+        options={{
+          title: "Report",
+          tabBarIcon: ({ color }) => <Icons.ReportIcon color={color} />,
+        }}
       />
     </Tabs>
   );
--- constants/colors.ts
+++ constants/colors.ts
@@ -1,4 +1,5 @@
 export const COLORS = {
   tabBarActiveTintColor: "#121212",
   tabBarInactiveTintColor: "#636363",
+  tabBarBackgroundColor: "#F1F1F1",
 };

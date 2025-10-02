c760cf8100abcfe8129d8f0718ba8391ed67d937
Hashan Danushka <73052374+hashan1998-it@users.noreply.github.com>
2025-10-02 10:49:57 +0530

    refactor layout components to use SafeAreaProvider and simplify index screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,14 @@
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
+    <SafeAreaView
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    </SafeAreaView>
   );
 }
